4ce532734d6c327743a00b2869ef5fdcb3e5d708
Yash-Handa <yashhanda7@yahoo.com>
2020-08-23 20:50:58 +0530

    updated help message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	helpFlag := getopt.Bool('?', "display help")
+	helpFlag := getopt.Bool('?', "display this help and exit")
 	_ = getopt.BoolLong("all", 'a', "do not ignore entries starting with .")
 	_ = getopt.BoolLong("almost-all", 'A', "do not list implied . and ..")
 	_ = getopt.Bool('1', "list one file per line.  Avoid '\\n' with -q or -b")
