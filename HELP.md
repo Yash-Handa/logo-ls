98acc04d5827b5c8495bd4ebe9c76b05c49ef6de
Yash-Handa <yashhanda7@yahoo.com>
2020-08-24 00:19:46 +0530

    Added most of the flags of ls

--- HELP.md
+++ HELP.md
@@ -1,9 +1,22 @@
 ```txt
-Usage: logo-ls [-1?aAdV] [parameters ...]
- -1                list one file per line.  Avoid '\n' with -q or -b
+Usage: logo-ls [-1?aAdgGhlorSstUvVX] [parameters ...]
+ -1                list one file per line.
  -?                display this help and exit
  -a, --all         do not ignore entries starting with .
  -A, --almost-all  do not list implied . and ..
  -d, --directory   list directories themselves, not their contents
+ -g                like -l, but do not list owner
+ -G, --no-group    in a long listing, don't print group names
+ -h, --human-readable
+                   with -l and -s, print sizes like 1K 234M 2G etc.
+ -l                use a long listing format
+ -o                like -l, but do not list group information
+ -r, --reverse     reverse order while sorting
+ -S                sort by file size, largest first
+ -s, --size        print the allocated size of each file, in blocks
+ -t                sort by modification time, newest first
+ -U                do not sort; list entries in directory order
+ -v                natural sort of (version) numbers within text
  -V, --version     output version information and exit
+ -X                sort alphabetically by entry extension
 ```
--- main.go
+++ main.go
@@ -7,14 +7,35 @@ import (
 )
 
 func main() {
-	helpFlag := getopt.Bool('?', "display this help and exit")
+	// content flags
 	_ = getopt.BoolLong("all", 'a', "do not ignore entries starting with .")
 	_ = getopt.BoolLong("almost-all", 'A', "do not list implied . and ..")
-	_ = getopt.Bool('1', "list one file per line.  Avoid '\\n' with -q or -b")
-	_ = getopt.BoolLong("version", 'V', "output version information and exit")
+
+	// display flags
+	_ = getopt.Bool('1', "list one file per line.")
 	_ = getopt.BoolLong("directory", 'd', "list directories themselves, not their contents")
+	_ = getopt.Bool('l', "use a long listing format")
+	_ = getopt.Bool('o', "like -l, but do not list group information")
+	_ = getopt.Bool('g', "\nlike -l, but do not list owner")
+	_ = getopt.BoolLong("no-group", 'G', "in a long listing, don't print group names")
+	_ = getopt.BoolLong("human-readable", 'h', "with -l and -s, print sizes like 1K 234M 2G etc.")
+	_ = getopt.BoolLong("size", 's', "print the allocated size of each file, in blocks") //use os.Getpagesize()
+
+	// sorting flags
+	_ = getopt.Bool('S', "sort by file size, largest first")
+	_ = getopt.Bool('U', "do not sort; list entries in directory order")
+	_ = getopt.Bool('X', "sort alphabetically by entry extension")
+	_ = getopt.Bool('v', "natural sort of (version) numbers within text")
+	_ = getopt.Bool('t', "sort by modification time, newest first")
+
+	_ = getopt.BoolLong("reverse", 'r', "reverse order while sorting")
+
+	helpFlag := getopt.Bool('?', "display this help and exit")
+	_ = getopt.BoolLong("version", 'V', "output version information and exit")
+
 	getopt.ParseV2()
 	if *helpFlag {
 		getopt.PrintUsage(os.Stdout)
+
 	}
 }
