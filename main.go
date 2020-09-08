e0b75464089e257a2128584ccdae787afc7882c1
Yash-Handa <yashhanda7@yahoo.com>
2020-09-09 01:20:54 +0530

    Bumped up version to 1.3.3

--- main.go
+++ main.go
@@ -142,7 +142,7 @@ func main() {
 
 	// if f_V is provided version will be printed and exit(0)
 	if *f_V {
-		fmt.Printf("logo-ls %s\nCopyright (c) 2020 Yash Handa\nLicense MIT <https://opensource.org/licenses/MIT>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n", "v1.3.0")
+		fmt.Printf("logo-ls %s\nCopyright (c) 2020 Yash Handa\nLicense MIT <https://opensource.org/licenses/MIT>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n", "v1.3.3")
 		fmt.Println("\nWritten by Yash Handa")
 		os.Exit(osExitCode)
 	}
